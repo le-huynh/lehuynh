9c95b7c787de642f6a147a65619f2313653f4663
le-huynh <lehuynh200194@gmail.com>
2024-01-31 15:20:03 +0700

    #1: update documentation (add examples + range of week value) + condition for year/week entry

--- R/tidytuesday.R
+++ R/tidytuesday.R
@@ -4,7 +4,7 @@
 #' _[#tidytuesday](https://github.com/rfordatascience/tidytuesday)_ challenge
 #'
 #' @param year numeric entry for year
-#' @param week the week of interest
+#' @param week numeric entry for the week of interest, from 1 to 52
 #'
 #' @return A folder containing folders and files for _#tidytuesday_ challenge.
 #' @export
@@ -15,9 +15,24 @@
 #' * different folders to hold all _data_, _code_, _plots_ of data analysis
 #' * templates
 #'
-tidytuesday = function(year, week)
-{
-          here::here()
+#' @examples
+#' \dontrun{
+#' tidytuesday(2023, 42)
+#'}
+
+
+tidytuesday = function(year, week){
+
+          is.wholenumber <- function(x, tol = .Machine$double.eps^0.5){
+                    abs(x - round(x)) < tol
+          }
+
+          stopifnot(is.wholenumber(year), is.wholenumber(week))
+          stopifnot(year >= 0, week >= 1, week <=52)
+
+          if(!dir.exists(paste0(here::here(), "/", year))) {
+                    dir.create(paste0(here::here(), "/", year))
+          }
 
           dir_name <- paste0(year, "/", year, "_week_", week, "/")
 
